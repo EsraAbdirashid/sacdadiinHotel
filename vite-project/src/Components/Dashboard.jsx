c3bc52425630383bee7993f5c251034bb97a5942
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-25 16:54:31 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { RiHotelLine } from "react-icons/ri";
 
 const Dashboard = () => {
   return (
@@ -12,7 +13,11 @@ const Dashboard = () => {
         <div className=" bg-slate-700 text-white w-[200px] h-[400px]">
                 <nav className='p-1 translate-x-7'>
                         <ul className='bg-white text-gray-700 rounded-full w-32 text-center font-bold border-2 hover:bg-slate-400 hover:text-white'>
-                                <Link to='/Hotel'>Hotel</Link>
+                                <Link to='/Hotel'>
+                              
+                              <RiHotelLine className='inline-block -translate-x-2'/>
+                              
+                                        Hotel</Link>
                         </ul>
                         <ul  className='bg-white text-gray-700 rounded-full w-32 text-center font-bold mt-2 border-2 hover:bg-slate-400 hover:text-white'>
                                 <Link to='/Employe'>Employe</Link>
