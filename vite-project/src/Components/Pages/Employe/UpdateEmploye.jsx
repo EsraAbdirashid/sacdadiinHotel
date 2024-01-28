88fa8cdcd7240cee24936faf5b67c98ef79b5eab
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-28 06:39:29 +0300

    Done

--- vite-project/src/Components/Pages/Employe/UpdateEmploye.jsx
+++ vite-project/src/Components/Pages/Employe/UpdateEmploye.jsx
@@ -13,11 +13,15 @@ const Post = () => {
         </div>
         <h2 className='text-4xl text-center font-bold uppercase'>Update Employe</h2>
         <form className=''>
-                <input type="text" placeholder='FirstName'className='px-5 p-2 mt-2 m-2 outline-none' required />
-                <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> <br />
+                <input type="text" placeholder='firstName'className='px-5 p-2 mt-2 m-2 outline-none' required />
+                <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> 
                 <input type="text" placeholder='LastName' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="text" placeholder='' className='ml-2 px-5 p-2  outline-none' required /><br />
-                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[150px]' />
+                <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="Number" placeholder='Phonee' className='ml-2 px-5 p-2 mt-2 outline-none' required />
+                <input type="text" placeholder='Position' className='ml-4 px-5 p-2 mt-2  outline-none' required />
+                <input type="number" placeholder='Salery' className='ml-2 px-5 p-2 mt-2  outline-none' required />
+                <input type="number" placeholder='Hotel_id' className='ml-4 px-5 p-2 mt-3 outline-none' required />
+                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
 
         </form>
        </div>
