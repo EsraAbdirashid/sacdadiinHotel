14749b5409c3952353709acd26150b3d356a099a
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-28 06:34:41 +0300

    Done

---
+++ vite-project/src/Components/Pages/Employe/UpdateEmploye.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { IoIosArrowBack } from "react-icons/io";
+
+const Post = () => {
+  return (
+    <div>
+       <div className="container -translate-y-[530px] translate-x-[500px] shadow shadow-slate-300 w-[500px]  h-[400px] bg-slate-200">
+        <div className='text-4xl'>
+                <Link to='/Hotel'>
+                <IoIosArrowBack />
+                </Link>
+        </div>
+        <h2 className='text-4xl text-center font-bold uppercase'>Update Hotel</h2>
+        <form className=''>
+                <input type="text" placeholder='HotelName'className='px-5 p-2 mt-2 m-2 outline-none' required />
+                <input type="text" placeholder='HotelName' className='ml-2 px-5 p-2  outline-none' required /> <br />
+                <input type="text" placeholder='HotelName' className='px-5 p-2 m-2  outline-none' required /> 
+                <input type="text" placeholder='HotelName' className='ml-2 px-5 p-2  outline-none' required /><br />
+                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[150px]' />
+
+        </form>
+       </div>
+    </div>
+  )
+}
+
+export default Post
