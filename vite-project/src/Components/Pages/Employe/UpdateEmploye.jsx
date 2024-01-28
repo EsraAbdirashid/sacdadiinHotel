af1a52e89b9b3b9e89f99223804ca331b86eb55a
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-28 07:03:52 +0300

    Done

--- vite-project/src/Components/Pages/Employe/UpdateEmploye.jsx
+++ vite-project/src/Components/Pages/Employe/UpdateEmploye.jsx
@@ -7,21 +7,17 @@ const UpdateEmploye = () => {
     <div>
        <div className="container -translate-y-[530px] translate-x-[500px] shadow shadow-slate-300 w-[500px]  h-[400px] bg-slate-200">
         <div className='text-4xl'>
-                <Link to='/Employe'>
+                <Link to='/PostEmploye'>
                 <IoIosArrowBack />
                 </Link>
         </div>
         <h2 className='text-4xl text-center font-bold uppercase'>Update Employe</h2>
         <form className=''>
-                <input type="text" placeholder='firstName'className='px-5 p-2 mt-2 m-2 outline-none' required />
-                <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> 
-                <input type="text" placeholder='LastName' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
-                <input type="Number" placeholder='Phonee' className='ml-2 px-5 p-2 mt-2 outline-none' required />
-                <input type="text" placeholder='Position' className='ml-4 px-5 p-2 mt-2  outline-none' required />
-                <input type="number" placeholder='Salery' className='ml-2 px-5 p-2 mt-2  outline-none' required />
-                <input type="number" placeholder='Hotel_id' className='ml-4 px-5 p-2 mt-3 outline-none' required />
-                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
+                <input type="text" placeholder='HotelName'className='px-5 p-2 mt-2 m-2 outline-none' required />
+                <input type="text" placeholder='Address' className='ml-2 px-5 p-2  outline-none' required /> <br />
+                <input type="text" placeholder='Email' className='px-5 p-2 m-2  outline-none' required /> 
+                <input type="text" placeholder='phone' className='ml-2 px-5 p-2  outline-none' required /><br />
+                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[150px]' />
 
         </form>
        </div>
@@ -29,4 +25,4 @@ const UpdateEmploye = () => {
   )
 }
 
-export default UpdateEmploye;
+export default UpdateEmploye
