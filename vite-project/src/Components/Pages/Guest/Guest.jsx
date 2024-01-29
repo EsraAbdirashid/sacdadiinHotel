4a0b1fc49c75067e24678221c671aa40b2e2652b
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-29 19:53:16 +0300

    Done

--- vite-project/src/Components/Pages/Guest/Guest.jsx
+++ vite-project/src/Components/Pages/Guest/Guest.jsx
@@ -4,26 +4,26 @@ import { IoAddCircleSharp } from "react-icons/io5";
 
 const Guest = () => {
   const handleDelete=()=>{
-    alert("Deleted Employe?!")
+    alert("Deleted Guest?!")
   }
   return (
     <div>
       <div className="container -translate-y-[550px]">
       <div className="add ml-[1200px] text-4xl bg-gray-600 text-white rounded-full w-9 space-x-3">
-        <Link to='/PostEmploye'>
+        <Link to='/PostGuest'>
         <IoAddCircleSharp />
         </Link>
       </div>
-   <table className='translate-x-[145px]  '>
+   <table className='translate-x-[200px]  '>
           <thead className='bg-gray-500 text-white'>
             <tr>
-              <th className='w-[90px]'>Employe_id</th>
-              <th className='w-[90px]'>firstName</th>
-              <th className='w-[90px]'>MiddleName</th>
-              <th className='w-[90px]'>LastName</th>
-              <th className='w-[90px]'>Email</th>
-              <th className='w-[90px]'>Phone</th>
-              <th className='w-[90px]'>Actions</th>
+              <th className='w-[130px]'>Guest_id</th>
+              <th className='w-[130px]'>firstName</th>
+              <th className='w-[130px]'>MiddleName</th>
+              <th className='w-[130px]'>LastName</th>
+              <th className='w-[130px]'>Email</th>
+              <th className='w-[130px]'>Phone</th>
+              <th className='w-[130px]'>Actions</th>
             </tr>
           </thead>
           <tbody className='bg-gray-300 translate-y-2 text-gray-700'>
@@ -35,7 +35,7 @@ const Guest = () => {
               <td className='text-center'>Esra@gmail.com</td>
               <td className='text-center'>+251921236763</td>
               <div className="buttons flex text-white">
-             <Link to='/UpdateEmploye'> <button className='bg-green-600  px-3 border-2 p-1'>Update</button></Link>
+             <Link to='/UpdateGuest'> <button className='bg-green-600  px-3 border-2 p-1'>Update</button></Link>
             <button className='bg-red-600 px-3 border-2 p-1 ' onClick={handleDelete}>Delete</button>
               </div>
               
