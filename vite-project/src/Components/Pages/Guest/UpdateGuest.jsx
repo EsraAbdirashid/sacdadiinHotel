2162e36c7498ee769811ebe2e5d9fea6f93cce89
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-29 20:04:28 +0300

    Done

--- vite-project/src/Components/Pages/Guest/UpdateGuest.jsx
+++ vite-project/src/Components/Pages/Guest/UpdateGuest.jsx
@@ -17,7 +17,7 @@ const UpdateGuest = () => {
                 <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> 
                 <input type="text" placeholder='LastName' className='px-5 p-2 m-2  outline-none' required /> 
                 <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
-                <input type="Number" placeholder='Phone' className='ml-2 px-5 p-2 mt-2 outline-none' required />
+                <input type="Number" placeholder='Phone' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
                 
                 <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
 
