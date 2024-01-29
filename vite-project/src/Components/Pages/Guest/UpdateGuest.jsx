c8ac41d9a9ee9d71c8f107cf8ef20ba7dbbf026a
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-29 19:58:10 +0300

    Done

---
+++ vite-project/src/Components/Pages/Guest/UpdateGuest.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { IoIosArrowBack } from "react-icons/io";
+
+const PostEmploye = () => {
+  return (
+    <div>
+       <div className="container -translate-y-[560px] translate-x-[500px] shadow shadow-slate-300 w-[500px]  h-[470px] bg-slate-200">
+        <div className='text-4xl'>
+                <Link to='/Employe'>
+                <IoIosArrowBack />
+                </Link>
+        </div>
+        <h2 className='text-4xl text-center font-bold uppercase'>Update Employe</h2>
+        <form className=''>
+                <input type="text" placeholder='firstName'className='px-5 p-2 mt-2 m-2 outline-none' required />
+                <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> 
+                <input type="text" placeholder='LastName' className='px-5 p-2 m-2  outline-none' required /> 
+                <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="Number" placeholder='Phonee' className='ml-2 px-5 p-2 mt-2 outline-none' required />
+                <input type="text" placeholder='Position' className='ml-4 px-5 p-2 mt-2  outline-none' required />
+                <input type="number" placeholder='Salery' className='ml-2 px-5 p-2 mt-2  outline-none' required />
+                <input type="number" placeholder='Hotel_id' className='ml-4 px-5 p-2 mt-3 outline-none' required />
+                <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
+
+        </form>
+       </div>
+    </div>
+  )
+}
+
+export default PostEmploye
