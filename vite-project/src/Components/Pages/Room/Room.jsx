8a125635046eb006318bac83865c249432a20e1d
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 19:35:47 +0300

    DOne

--- vite-project/src/Components/Pages/Room/Room.jsx
+++ vite-project/src/Components/Pages/Room/Room.jsx
@@ -16,7 +16,7 @@ const Room = () => {
       </div>
    <table className='translate-x-[200px]  '>
           <thead className='bg-gray-500 text-white'>
-            {/* <tr>
+            <tr>
               <th className='w-[130px]'>Room_id</th>
               <th className='w-[130px]'>RoomNumber</th>
               <th className='w-[130px]'>Guest_id</th>
@@ -25,7 +25,7 @@ const Room = () => {
               <th className='w-[130px]'>CheckoutData</th>
               <th className='w-[130px]'>Actions</th>
             </tr>
-          </thead>
+          {/* </thead>
           <tbody className='bg-gray-300 translate-y-2 text-gray-700'>
             <tr className=''>
               <td className='text-center'>1</td>
@@ -39,8 +39,8 @@ const Room = () => {
             <button className='bg-red-600 px-3 border-2 p-1 ' onClick={handleDelete}>Delete</button>
               </div>
               
-            </tr> */}
-          </tbody>
+            </tr>
+          </tbody> */}
         </table>
       </div>
     </div>
