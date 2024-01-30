982af35fb894af9d81fac24be6265105857f145d
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 19:27:36 +0300

    DOne

--- vite-project/src/Components/Pages/Booking/UpdateBooking.jsx
+++ vite-project/src/Components/Pages/Booking/UpdateBooking.jsx
@@ -16,8 +16,8 @@ const UpdateBooking = () => {
                 <input type="number" placeholder='RoomNumber'className='px-5 p-2 mt-2 m-2 outline-none' required />
                 <input type="number" placeholder='Guest_id' className='ml-2 px-5 p-2  outline-none' required /> 
                 <input type="Number" placeholder='TotalPrice' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
-                <input type="Number" placeholder='Phone' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
+                <input type="Date" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="Date" placeholder='Phone' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
                 
                 <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
 
