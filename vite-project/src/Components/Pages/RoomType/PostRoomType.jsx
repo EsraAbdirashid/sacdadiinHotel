4b51a2a4290d55754ae959955a81d21f1fa01e33
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 19:58:34 +0300

    DOne

--- vite-project/src/Components/Pages/RoomType/PostRoomType.jsx
+++ vite-project/src/Components/Pages/RoomType/PostRoomType.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { IoIosArrowBack } from "react-icons/io";
 
-const PostRoom = () => {
+const PostRoomType = () => {
   return (
     <div>
        <div className="container -translate-y-[560px] translate-x-[500px] shadow shadow-slate-300 w-[500px]  h-[470px] bg-slate-200">
         <div className='text-4xl'>
-                <Link to='/Room'>
+                <Link to='/RoomType'>
                 <IoIosArrowBack />
                 </Link>
         </div>
         <h2 className='text-4xl text-center font-bold uppercase'>REgistration Room</h2>
         <form className=''>
-                <input type="number" placeholder='Hotel_id'className='px-5 p-2 mt-2 m-2 outline-none' required />
-                <input type="number" placeholder='Type_id' className='ml-2 px-5 p-2  outline-none' required /> 
-                <input type="number" placeholder='Booking_id' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="text" placeholder='status' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="number" placeholder='Name'className='px-5 p-2 mt-2 m-2 outline-none' required />
+                <input type="number" placeholder='priceperNight' className='ml-2 px-5 p-2  outline-none' required /> 
+                <input type="number" placeholder='Description' className='px-5 p-2 m-2  outline-none' required /> 
+                <input type="text" placeholder='Capacity' className='ml-2 px-5 p-2  outline-none' required />
               
                
                 <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
@@ -27,4 +27,4 @@ const PostRoom = () => {
   )
 }
 
-export default PostRoom
+export default PostRoomType
