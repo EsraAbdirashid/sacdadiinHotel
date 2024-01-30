514471cb2015ab7173a80d14795d2b8a2a86ddf8
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 19:21:18 +0300

    DOne

--- vite-project/src/Components/Pages/Booking/PostBooking.jsx
+++ vite-project/src/Components/Pages/Booking/PostBooking.jsx
@@ -16,8 +16,8 @@ const PostBooking = () => {
                 <input type="number" placeholder='RoomNumber'className='px-5 p-2 mt-2 m-2 outline-none' required />
                 <input type="number" placeholder='Guest_id' className='ml-2 px-5 p-2  outline-none' required /> 
                 <input type="number" placeholder='TotalPrice' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="Date" placeholder='checkinData' className='ml-2 px-5 p-2  outline-none' required />
-                <input type="Date" placeholder='checkoutData' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
+                <input type="Date" placeholder='checkinDate' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="Date" placeholder='checkoutDate' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
                
                 <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
 
