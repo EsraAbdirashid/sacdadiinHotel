7a14a9de3ca4bffdff2b3341bb03a886aad0ba34
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 18:24:28 +0300

    DOne

--- vite-project/src/Components/Pages/Booking/PostBooking.jsx
+++ vite-project/src/Components/Pages/Booking/PostBooking.jsx
@@ -14,10 +14,10 @@ const PostBooking = () => {
         <h2 className='text-4xl text-center font-bold uppercase'>REgistration Booking</h2>
         <form className=''>
                 <input type="number" placeholder='RoomNumber'className='px-5 p-2 mt-2 m-2 outline-none' required />
-                <input type="text" placeholder='MiddleName' className='ml-2 px-5 p-2  outline-none' required /> 
-                <input type="text" placeholder='LastName' className='px-5 p-2 m-2  outline-none' required /> 
-                <input type="text" placeholder='Email' className='ml-2 px-5 p-2  outline-none' required />
-                <input type="Number" placeholder='Phone' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
+                <input type="number" placeholder='Guest_id' className='ml-2 px-5 p-2  outline-none' required /> 
+                <input type="number" placeholder='TotalPrice' className='px-5 p-2 m-2  outline-none' required /> 
+                <input type="Date" placeholder='checkinData' className='ml-2 px-5 p-2  outline-none' required />
+                <input type="Date" placeholder='checkoutData' className='ml-[109px] px-5 p-2 mt-2 outline-none' required />
                
                 <input type="submit" className='bg-green-600 px-[205px] p-3 rounded-2xl text-white hover:bg-green-900 font-bold m-2  mt-[100px]' />
 
