bf29a8be0f8bd6fa4c1f4bce570d1ab88ee3756b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-01 15:43:07 +0300

    Done

--- vite-project/src/Components/Pages/Payment/Payment.jsx
+++ vite-project/src/Components/Pages/Payment/Payment.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { IoAddCircleSharp } from "react-icons/io5";
 
-const Payment = () => {
+const RoomType = () => {
+  const handleDelete=()=>{
+    alert("Deleted RoomType?!")
+  }
   return (
-    <div  className='-translate-y-[270px] translate-x-[370px] w-[400px] text-black bg-slate-300 border-2 p-5 rounded-lg'>Payment</div>
+    <div>
+      <div className="container -translate-y-[550px]">
+      <div className="add ml-[1200px] text-4xl bg-gray-600 text-white rounded-full w-9 space-x-3">
+        <Link to='/PostRoomType'>
+        <IoAddCircleSharp />
+        </Link>
+      </div>
+   <table className='translate-x-[200px]  '>
+          <thead className='bg-gray-500 text-white'>
+            <tr>
+              <th className='w-[130px]'>RoomType_id</th>
+              <th className='w-[130px]'>Name</th>
+              <th className='w-[130px]'>PriceperNight_id</th>
+              <th className='w-[130px]'>Description</th>
+              <th className='w-[130px]'>Capacity</th>
+              <th className='w-[130px]'>Actions</th>
+            </tr>
+          </thead>
+          <tbody className='bg-gray-300 translate-y-2 text-gray-700'>
+            <tr className=''>
+              <td className='text-center'>1</td>
+              <td className='text-center'>Miss Esra</td>
+              <td className='text-center'>9000</td>
+              <td className='text-center'>waysoo dagtay</td>
+              <td className='text-center'>single</td>
+              <div className="buttons flex text-white">
+             <Link to='/UpdateRoomType'> <button className='bg-green-600  px-3 border-2 p-1'>Update</button></Link>
+            <button className='bg-red-600 px-3 border-2 p-1 ' onClick={handleDelete}>Delete</button>
+              </div>
+              
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
-export default Payment
+export default RoomType
